db16be9b023db49e6b9b328f7d228016370b7dbf
dokudo91 <92647519+dokudo91@users.noreply.github.com>
2024-09-03 13:54:09 +0900

    型安定コードに修正

--- src/auth.jl
+++ src/auth.jl
@@ -2,10 +2,24 @@ using PythonCall, CondaPkg
 #]conda add google-auth google-auth-oauthlib google-auth-httplib2 google-api-python-client
 using JSON3
 
-function auth(secret, scopes)
+function auth(secretpath, tokenpath, scopes)
+    if isfile(tokenpath)
+        Credentials = pyimport("google.oauth2.credentials").Credentials
+        credentials = Credentials.from_authorized_user_file(tokenpath, scopes |> pylist)
+        secretjson = JSON3.read(read(secretpath))
+        if string(credentials.client_id::Py) == secretjson.installed.client_id::String
+            if pynot(credentials.valid)
+                if pytruth(credentials.expired) && pytruth(credentials.refresh_token)
+                    Request = pyimport("google.auth.transport.requests").Request
+                    credentials.refresh(Request())
+                end
+            end
+            return credentials
+        end
+    end
     InstalledAppFlow = pyimport("google_auth_oauthlib.flow").InstalledAppFlow
-    flow = InstalledAppFlow.from_client_secrets_file(secret, scopes |> pylist)
-    flow.run_local_server(port=0)
+    flow = InstalledAppFlow.from_client_secrets_file(secretpath, scopes |> pylist)
+    return flow.run_local_server(port=0)
 end
 """
     build_service(secretpath, tokenpath, scopes, name, version)
@@ -20,29 +34,10 @@ build_service(secretpath, tokenpath, scopes, name, version)
 ```
 """
 function build_service(secretpath, tokenpath, scopes, name, version)
-    Credentials = pyimport("google.oauth2.credentials").Credentials
-    Request = pyimport("google.auth.transport.requests").Request
-    build = pyimport("googleapiclient.discovery").build
-
-    secretjson = JSON3.read(read(secretpath))
-    if isfile(tokenpath)
-        credentials = Credentials.from_authorized_user_file(tokenpath, scopes |> pylist)
-        if credentials.client_id |> string == secretjson.installed.client_id
-            if pynot(credentials.valid)
-                if pytruth(credentials.expired) && pytruth(credentials.refresh_token)
-                    credentials.refresh(Request())
-                else
-                    credentials = auth(secretpath, scopes)
-                end
-            end
-        else
-            credentials = auth(secretpath, scopes)
-        end
-    else
-        credentials = auth(secretpath, scopes)
-    end
+    credentials = auth(secretpath, tokenpath, scopes)
     open(tokenpath, "w") do io
         write(io, credentials.to_json() |> string)
     end
+    build = pyimport("googleapiclient.discovery").build
     build(name, version; credentials)
 end
