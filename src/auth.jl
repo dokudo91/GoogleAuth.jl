e80b6748b0e0fa2c80792b5299a7924ec93823b8
dokudo91 <92647519+dokudo91@users.noreply.github.com>
2024-09-04 14:01:00 +0900

    PyCallを使うように変更

--- src/auth.jl
+++ src/auth.jl
@@ -1,25 +1,30 @@
-using PythonCall, CondaPkg
-#]conda add google-auth google-auth-oauthlib google-auth-httplib2 google-api-python-client
+using PyCall
+using PyCallUtils
 using JSON3
+
+pyimport_Credentials() = pyimport_object("google.oauth2.credentials", "google-auth", :Credentials)
+pyimport_Request() = pyimport_object("google.auth.transport.requests", "google-auth", :Request)
+pyimport_InstalledAppFlow() = pyimport_object("google_auth_oauthlib.flow", "google-auth-oauthlib", :InstalledAppFlow)
+pyimport_build() = pyimport_object("googleapiclient.discovery", "google-api-python-client", :build)
 
 function auth(secretpath, tokenpath, scopes)
     if isfile(tokenpath)
-        Credentials = pyimport("google.oauth2.credentials").Credentials
-        credentials = Credentials.from_authorized_user_file(tokenpath, scopes |> pylist)
+        Credentials = pyimport_Credentials()
+        credentials = Credentials.from_authorized_user_file(tokenpath, scopes)::PyObject
         secretjson = JSON3.read(read(secretpath))
-        if string(credentials.client_id::Py) == secretjson.installed.client_id::String
-            if pynot(credentials.valid)
-                if pytruth(credentials.expired) && pytruth(credentials.refresh_token)
-                    Request = pyimport("google.auth.transport.requests").Request
+        if credentials.client_id::String == secretjson.installed.client_id::String
+            if credentials.valid::Bool
+                if credentials.expired::Bool && !isnothing(credentials.refresh_token::Union{Nothing,String})
+                    Request = pyimport_Request()
                     credentials.refresh(Request())
                 end
             end
             return credentials
         end
     end
-    InstalledAppFlow = pyimport("google_auth_oauthlib.flow").InstalledAppFlow
-    flow = InstalledAppFlow.from_client_secrets_file(secretpath, scopes |> pylist)
-    return flow.run_local_server(port=0)
+    InstalledAppFlow = pyimport_InstalledAppFlow()
+    flow = InstalledAppFlow.from_client_secrets_file(secretpath, scopes)::PyObject
+    return flow.run_local_server(port=0)::PyObject
 end
 """
     build_service(secretpath, tokenpath, scopes, name, version)
@@ -36,10 +41,10 @@ build_service(secretpath, tokenpath, scopes, name, version)
 function build_service(secretpath, tokenpath, scopes, name, version)
     credentials = auth(secretpath, tokenpath, scopes)
     open(tokenpath, "w") do io
-        write(io, credentials.to_json() |> string)
+        write(io, credentials.to_json())
     end
-    build = pyimport("googleapiclient.discovery").build
-    build(name, version; credentials)
+    build = pyimport_build()
+    build(name, version; credentials)::PyObject
 end
 function build_service(secretpath, scopes, name, version)
     tokenpath = create_tokenpath(secretpath)
