cd528635e3dee1248d24e034ca15694d8a628695
dokudo91 <92647519+dokudo91@users.noreply.github.com>
2024-09-25 13:06:53 +0900

    authの修正

--- src/auth.jl
+++ src/auth.jl
@@ -7,25 +7,35 @@ pyimport_Request() = pyimport_object("google.auth.transport.requests", "google-a
 pyimport_InstalledAppFlow() = pyimport_object("google_auth_oauthlib.flow", "google-auth-oauthlib", :InstalledAppFlow)
 pyimport_build() = pyimport_object("googleapiclient.discovery", "google-api-python-client", :build)
 
+"""
+```
+secretpath = "client_secret.json"
+tokenpath = "token.json"
+scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
+auth(secretpath, tokenpath, scopes)
+```
+"""
 function auth(secretpath, tokenpath, scopes)
-    if isfile(tokenpath)
-        Credentials = pyimport_Credentials()
-        credentials = Credentials.from_authorized_user_file(tokenpath, scopes)::PyObject
-        secretjson = JSON3.read(read(secretpath))
-        if credentials.client_id::String == secretjson.installed.client_id::String
-            if credentials.valid::Bool
-                if credentials.expired::Bool && !isnothing(credentials.refresh_token::Union{Nothing,String})
+    try
+        if isfile(tokenpath)
+            Credentials = pyimport_Credentials()
+            credentials = Credentials.from_authorized_user_file(tokenpath, scopes)::PyObject
+            secretjson = JSON3.read(read(secretpath))
+            if credentials.client_id::String == secretjson.installed.client_id::String
+                if !credentials.valid::Bool
                     Request = pyimport_Request()
                     credentials.refresh(Request())
                 end
+                return credentials
             end
-            return credentials
         end
+    catch
     end
     InstalledAppFlow = pyimport_InstalledAppFlow()
     flow = InstalledAppFlow.from_client_secrets_file(secretpath, scopes)::PyObject
     return flow.run_local_server(port=0)::PyObject
 end
+
 """
     build_service(secretpath, tokenpath, scopes, name, version)
 
